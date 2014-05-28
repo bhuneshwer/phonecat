0cff57aa3215a43b7e1f01f391eb39c578465c85
bhuneshwer <bhunesh11@gmail.com>
2014-05-28 10:13:36 +0530

    Update services.js

--- phonecat_ex/src/main/webapp/app/js/services.js
+++ phonecat_ex/src/main/webapp/app/js/services.js
@@ -14,7 +14,7 @@ phonecatServices.factory('Phone', ['$resource','$http',
 phonecatServices.factory('Phone', ['$resource','$http','$routeParams',
                                    function($resource,$http,$routeParams,event){
  	return $resource('../phonegallarys/:id', {}, {
-	      query1: {method:'GET', params:{'id':$routParams.id}, isArray:false}
+	      query1: {method:'GET', params:{'id':$routeParams.id}, isArray:false}
 	
 	    });
 }]);
